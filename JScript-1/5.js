ba2e696b362aecad3675848bb652ea0b1b476221
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2016-12-16 20:34:30 +0300

    Пятое ДЗ
    поправил задание со звездой, надо переписать первое и второе еще

--- JScript-1/5.js
+++ JScript-1/5.js
@@ -1,22 +1,33 @@
 // Первая часть
 // Функция преобразования цвета из десятичного формата в шестнадцатеричный.
+//Оптимизировать
 function colorChanger(r, g, b){
 	var color;
-	r = r.toString(16);
-	g = g.toString(16);
-	b = b.toString(16);
-	if (r == 0){
-		r = "00";
+	// r = r.toString(16);
+	// g = g.toString(16);
+	// b = b.toString(16);
+	function valid(){
+		if (clr < 0 || clr > 255){
+			return console.log("Некорректный ввод цвета. Вводить нужно от 0 до 255");
+		}
+
+		var clr = (clr.length<2) ? "0" + clr : clr;
+
+		return clr;
 	}
-	if (g == 0){
-		g = "00";
-	}
-	if (b == 0){
-		b = "00";
-	}
-	r = (r.length < 2) ? "0"+r : r;
-	g = (g.length < 2) ? "0"+g : g;
-	b = (b.length < 2) ? "0"+b : b;
+	
+	// if (r == 0){
+	// 	r = "00";
+	// }
+	// if (g == 0){
+	// 	g = "00";
+	// }
+	// if (b == 0){
+	// 	b = "00";
+	// }
+	// r = (r.length < 2) ? "0"+r : r;
+	// g = (g.length < 2) ? "0"+g : g;
+	// b = (b.length < 2) ? "0"+b : b;
 	return color = "#" + r + g + b;
 }
 color = colorChanger(117, 0, 5);
@@ -24,38 +35,38 @@ console.log(color);
 
 // Вторая часть
 // Преобразовать число в объект. Вводить можно от 0 до 999, если больше то вернуть пустой объект. 
-function numToObj(){		
-	if(num.length > 3){
+function numToObj(num){		
+	if(num < 0 || num > 999){
 		console.log("Число не должно превышать 999");
 		return {}
 	}
-	var obj = {}
-	if(num.length == 2){
-		num = "0" + num;
-	}
-	if(num.length == 1){
-		num = "00" + num;
-	}
-	obj['Единицы'] = num[2];
-	obj['Десятки'] = num[1];
-	obj['Сотни'] = num[0];	
-	return obj;
+	// var obj = {}
+	// if(num.length == 2){
+	// 	num = "0" + num;
+	// }
+	// if(num.length == 1){
+	// 	num = "00" + num;
+	// }
+	// obj['Единицы'] = num[2];
+	// obj['Десятки'] = num[1];
+	// obj['Сотни'] = num[0];	
+	// return obj;
 }
-var num = prompt("Введите число");
-while (num.replace (/\d/g, '').length || num == "") { // Проверку на "не числа" нашел в интернете
-	num = prompt ('Вводить можно только числа');
-}
-console.log(numToObj(num));
+// var num = prompt("Введите число");
+// while (num.replace (/\d/g, '').length || num == "") { // Проверку на "не числа" нашел в интернете
+// 	num = prompt ('Вводить можно только числа');
+// }
+console.log(numToObj(prompt("Введите число")));
 
 // Задание со звездочкой
 // * Реализовать функцию objectToQueryString(object), которая принимает аргументом объект, возвращает строку
 
 function objectToQueryString(obj){
-	var string = '';
+	var string = [];
 	for(var key in obj){ // в "map" не разобрался пока, реализовал через "in"
-		string += key + "=" + obj[key] + "&" ;
+		string.push(key + "=" + obj[key]);
 	}
-	return string;
+	return string.join('&');
 }	
 
 console.log(objectToQueryString({user: 'Dmitry'})); // user=Dmitry
