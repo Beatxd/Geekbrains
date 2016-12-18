8359dbff842b0b8b66af559ebd8cf487ca88374b
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2016-12-18 21:39:45 +0300

    Шестое ДЗ
    Вторая часть

--- JScript-1/6.js
+++ JScript-1/6.js
@@ -15,20 +15,22 @@ var list = {
 		}
 	}
 }
-
+var array = [1,2,3,4,7,8];
+// Из массива в список (не работает)
+function arrayToList(arr){
+	var list = {};
+	for(var i = 0; arr.length > i; i++){
+		function func(){
+			list.value = arr[i];
+			list.rest = func();
+		}
+	return list;
+	}
+}
+// Из списка в массив 
 function listToArray(obj){
 	var arr = [];
-	
 	function toArr(obj){
-		// for(var key in obj){
-		// while(obj[key] != null){
-		// 	if(typeof obj[key] == "object"){
-		// 		toArr(obj[key]);
-		// 	}
-		// 	arr.push(obj[key]);
-		// 	return arr;
-		// 	}
-		// }
 		for(var key in obj){
 			if(obj[key] == null){
 				return;
@@ -38,20 +40,42 @@ function listToArray(obj){
 			}
 		arr.push(obj[key]);
 		}
-		
 	}
 	toArr(obj);
+	arr.length = (arr.length + 1) / 2; // Костыль убирающий лишние элементы из массива.
 	return arr;
 }
 list = listToArray(list);
 console.log(list);
+console.log(arrayToList(array));
 
-// myObject = { 
-// 	'a': 1, 
-// 	'b': 2, 
-// 	'c': 3 
-// }
-// Object.keys(myObject).map(function(key, index) {
-//    myObject[key] *= 2;
-// });
-// console.log(myObject);
+// Вторая часть
+// Напишите две функции, reverseArray и reverseArrayInPlace. Первая получает массив как
+// аргумент и выдаёт новый массив, с обратным порядком элементов. Вторая работает как
+// оригинальный метод reverse – она меняет порядок элементов на обратный в том массиве,
+// который был ей передан в качестве аргумента. Не используйте стандартный метод reverse.
+function reverseArray(arr){
+	newArr = [];
+	for(i = 0; arr.length > i; i++){
+		var temp = arr.pop(i);
+		newArr.push(temp);
+		arr.unshift(temp);
+	}
+	return newArr;
+}
+var array2 = [1,2,3,4,7];
+var newArray = reverseArray(array2);
+console.log(newArray); // [7, 4, 3, 2, 1]
+//console.log(array2); // проверка корректности первоначального массива [1, 2, 3, 4, 7]
+function reverseArrayInPlace(arr){
+	var temp = []
+	for(i = 0; arr.length > i; i){
+		temp.push(arr.pop(i));
+	}
+	for(i = 0; temp.length > i; i){
+		arr.push(temp.shift(i));
+	}
+}
+var array3 = [9,8,7,6,5];
+reverseArrayInPlace(array3);
+console.log(array3); // [5, 6, 7, 8, 9]
