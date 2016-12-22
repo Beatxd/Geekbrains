a24db1ab80f473debd35dc50c5aa841c0bbe2884
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2016-12-23 02:05:04 +0300

    Седьмое ДЗ
    Исправил названия фигур

--- JScript-1/7.js
+++ JScript-1/7.js
@@ -10,14 +10,14 @@ function board() {
 	for (var i = 8; i >= 1; i--) {
 		var newTr = chess.lastChild.appendChild(document.createElement('tr'));
 		if (i == 1 || i == 2){
-			newTr.className = 'white';
+			newTr.className = 'white' // задаем цвета фигурам
 		}
 		if (i == 7 || i == 8){
 			newTr.className = 'black';
 		}
 		for (var i2 = 1; i2 <= 8; i2++) {
 			var newTd = chess.lastChild.lastChild.appendChild(document.createElement('td'));
-			newTd.id = arrSbl[i2-1] + i;
+			newTd.id = arrSbl[i2-1] + i; // присвоив айди каждой ячейке, облегчаем себе жизнь для любых задач с доской
 		}
 	}
 	var newSpan = chess.appendChild(document.createElement('span'));
@@ -36,7 +36,7 @@ function board() {
 		count.innerHTML = 'П';
 		count = count.nextElementSibling;
 	}
-	var figures = ['Т','Кн','Л','Ф','К','Kр','Kн','Т'];
+	var figures = ['Т','Кн','Л','Ф','Кр','Л','Kн','Т'];
 	a1.innerHTML = figures[0];
 	count = a1.nextElementSibling
 	for(var i = 1; i < 8; i++){
