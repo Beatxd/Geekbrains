fdab5e536dca15dee0dd3bac7286669f58970c9e
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2016-12-23 21:45:51 +0300

    Седьмое ДЗ
    Поменял фигуры

--- JScript-1/7.js
+++ JScript-1/7.js
@@ -15,6 +15,7 @@ function board() {
 		if (i == 7 || i == 8){
 			newTr.className = 'black';
 		}
+		
 		for (var i2 = 1; i2 <= 8; i2++) {
 			var newTd = chess.lastChild.lastChild.appendChild(document.createElement('td'));
 			newTd.id = arrSbl[i2-1] + i; // присвоив айди каждой ячейке, облегчаем себе жизнь для любых задач с доской
@@ -23,30 +24,31 @@ function board() {
 	var newSpan = chess.appendChild(document.createElement('span'));
 	newSpan.innerHTML = 'ABCDEFGH';
 	
-	// Вторая часть
-	a2.innerHTML = 'П';
+	// Вторая часть: Заполнение фигрурами
+	a2.innerHTML = '&#9817;';
 	var count = a2.nextElementSibling
 	for(var i = 0; i < 7; i++){
-		count.innerHTML = 'П';
+		count.innerHTML = '&#9817;';
 		count = count.nextElementSibling;
 	}
-	a7.innerHTML = 'П';
+	a7.innerHTML = '&#9823;';
 	count = a7.nextElementSibling
 	for(var i = 0; i < 7; i++){
-		count.innerHTML = 'П';
+		count.innerHTML = '&#9823;';
 		count = count.nextElementSibling;
 	}
-	var figures = ['Т','Кн','Л','Ф','Кр','Л','Kн','Т'];
-	a1.innerHTML = figures[0];
+	var figuresW = ['&#9814;','&#9816;','&#9815;','&#9813;','&#9812;','&#9815;','&#9816;','&#9814;'];
+	a1.innerHTML = figuresW[0];
 	count = a1.nextElementSibling
 	for(var i = 1; i < 8; i++){
-		count.innerHTML = figures[i];;
+		count.innerHTML = figuresW[i];;
 		count = count.nextElementSibling;
 	}
-	a8.innerHTML = figures[0];
+	figuresB = ['&#9820;','&#9822;','&#9821;','&#9819;','&#9818;','&#9821;','&#9822;','&#9820;'];
+	a8.innerHTML = figuresB[0];
 	count = a8.nextElementSibling
 	for(var i = 1; i < 8; i++){
-		count.innerHTML = figures[i];;
+		count.innerHTML = figuresB[i];;
 		count = count.nextElementSibling;
 	}
 }
