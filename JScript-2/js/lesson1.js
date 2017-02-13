2ee4860a1fea6a7d1b5c8ba543a64d50a735dea7
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-02-13 23:03:32 +0300

    lesson 1

--- JScript-2/js/lesson1.js
+++ JScript-2/js/lesson1.js
@@ -7,20 +7,54 @@
  Для генерации доски можно использовать произвольные html-тэги.
  Подумать какие свойства должны быть скрыты, а какие нет решение аргументировать
  в комментариях к коду. */
+// function CreateBoard() {
+//     var chessBoard = document.body.appendChild(document.createElement('div'));
+//     chessBoard.className='chessCreate';
+//     var letters = document.body.appendChild(document.createElement('div'));
+//     letters.className='chessLetter';
+//     letters.innerText = 'ABCDEFGH';
+//     var i = 0, count = 0;
+//     while (count < 64) {
+//         var cell = document.createElement('div');
+//         chessBoard.appendChild(cell);
+//         cell.className='cell';
+//         // if (i && i % 2) cell.className='blackCell';
+//         if (i && i % 2) cell.classList.add('blackCell');
+//         i += ((i + 2) % 9) ? 1 : 2;
+//         count++;
+//     }
+//
+// }
 function CreateBoard() {
     var chessBoard = document.body.appendChild(document.createElement('div'));
     chessBoard.className='chessCreate';
-// var chessBoard = document.querySelector('.chessCreate');
-    var i = 0, count = 0;
 
-    while (count < 64) {
+    var i = 0, count = 0;
+    var numberCount = 8;
+    var letterCount = 1;
+    var letters = ' ABCDEFGH';
+
+    while (count < 64 + 17) {
         var cell = document.createElement('div');
         chessBoard.appendChild(cell);
-        cell.classList.add('cell');
-        if (i && i % 2) cell.classList.add('blackCell');
-        i += ((i + 2) % 9) ? 1 : 2;
+        cell.className = 'cell';
+        // if (i && i % 2) cell.className='blackCell';
+        if (!(i && i % 2)) cell.classList.add('blackCell');
+        if (!((i + 9) % 9)) {
+            cell.classList.add('chessLetter');
+            if (numberCount > 0) cell.innerText = numberCount;
+            numberCount--;
+        }
+        if (i + 1 > 72) {
+            cell.classList.add('chessLetter');
+            if (letterCount <= 8) cell.innerText = letters[letterCount];
+            letterCount++;
+        }
+        // i += ((i + 2) % 70) ? 1 : 2;
+        i++;
         count++;
     }
+
 }
 CreateBoard();
 
