719fd812c7808a22b6aa4ad315b0f5f15ff58628
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2017-02-23 23:43:57 +0300

    Lesson4 (first task correction)

--- JScript-2/js/lesson4.js
+++ JScript-2/js/lesson4.js
@@ -1,34 +1,14 @@
 ;(function ($) {
     'use strict';
-    // $(".hr-menu").click(function(){clickOnTab()}); // не удается корректно передать параметры, поэтому сделал то что ниже с 10 строки
-    // function clickOnTab(tab, strCase) {
-    //     $('.hr-menu ~ div').hide(); // прячем все дивы с контентом
-    //     $("ul.hr-menu li").removeClass('active-tab'); // убираем со всех вкладок активность
-    //     $(strCase).show(); // показываем переданный див
-    //     tab.addClass('active-tab'); // делаем активной переданную вкладку
-    // }
-    $("#tab1").click(function () {
-        $('.hr-menu ~ div').hide();
-        $('#case1').show();
-        $("ul.hr-menu li").removeClass('active-tab');
-        $("#tab1").addClass('active-tab');
-    });
-    $("#tab2").click(function () {
-        $('.hr-menu ~ div').hide();
-        $('#case2').show();
-        $("ul.hr-menu li").removeClass('active-tab');
-        $("#tab2").addClass('active-tab');
-    });
-    $("#tab3").click(function () {
-        $('.hr-menu ~ div').hide();
-        $('#case3').show();
-        $("ul.hr-menu li").removeClass('active-tab');
-        $("#tab3").addClass('active-tab');
-    });
-    $("#tab4").click(function () {
-        $('.hr-menu ~ div').hide();
-        $('#case4').show();
-        $("ul.hr-menu li").removeClass('active-tab');
-        $("#tab4").addClass('active-tab');
-    });
+    $("#tab1").click(function(){clickOnTab('#tab1','#case1')});
+    $("#tab2").click(function(){clickOnTab('#tab2','#case2')});
+    $("#tab3").click(function(){clickOnTab('#tab3','#case3')});
+    $("#tab4").click(function(){clickOnTab('#tab4','#case4')});
+    function clickOnTab(tab, strCase) {
+        $('.hr-menu ~ div').hide(); // прячем все дивы с контентом
+        $("ul.hr-menu li").removeClass('active-tab'); // убираем со всех вкладок активность
+        $(strCase).show(); // показываем переданный див
+        $(tab).addClass('active-tab'); // делаем активной переданную вкладку
+    }
+
 })(jQuery);
