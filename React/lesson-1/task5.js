4a5277a837ad5e2142efa45b3d78f6a48db199a2
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-08 01:04:54 +0300

    lesson1 task5: UPD add arr of promises

--- React/lesson-1/task5.js
+++ React/lesson-1/task5.js
@@ -5,27 +5,23 @@
  вместо number подставляется число от 1 до 10, в итоге должно получиться 10 промисов.
  Следует дождаться выполнения загрузки всеми промисами и далее вывести массив
  загруженных данных
-*/
+ */
 const arr = [];
 
-function sendRequest(url, callback) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      arr.push(JSON.parse(this.responseText));
-      callback();
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-
 for (let i = 1; i <= 10; i++) {
-  let p = new Promise((resolve) => {
+  arr.push(new Promise((resolve) => {
     let url = "https://jsonplaceholder.typicode.com/users/" + i;
-    sendRequest(url, resolve);
-  });
-  p.then(() => {
-    if (arr.length === 10) console.log(arr);
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }));
+  Promise.all(arr).then(res => {
+    // без условия выполнится 10 раз. почему?
+    if (res.length === 10) console.log(res);
   })
 }
