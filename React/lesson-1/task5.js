9e7d2fc12c426c99635c51be495d9fc4f74343a5
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-07 20:35:59 +0300

    lesson1: task5 complete (without jQuery)

--- React/lesson-1/task5.js
+++ React/lesson-1/task5.js
@@ -6,15 +6,24 @@
  Следует дождаться выполнения загрузки всеми промисами и далее вывести массив
  загруженных данных
 */
-
 const arr = [];
+
+function sendRequest(url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      arr.push(JSON.parse(this.responseText));
+      callback();
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
 for (let i = 1; i <= 10; i++) {
   let p = new Promise((resolve) => {
     let url = "https://jsonplaceholder.typicode.com/users/" + i;
-    $.get(url, function (data) {
-      arr.push(data);
-      resolve();
-    });
+    sendRequest(url, resolve);
   });
   p.then(() => {
     if (arr.length === 10) console.log(arr);
