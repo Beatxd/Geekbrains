0d71577e906ee9fb1323d7960c88986e00357399
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-09 22:21:34 +0300

    lesson2 webpack: prepare structure

---
+++ React/lesson-2-webpack/components/user.js
@@ -0,0 +1,17 @@
+'use strict';
+class User {
+  constructor(name = null, department = null, salery = 30000) {
+    this.name = name;
+    this.department = department;
+    this.salery = salery
+  }
+
+  about {
+    return '';
+}
+}
+
+const user = {
+  'name': 'John',
+  'department': 'IT',
+};
