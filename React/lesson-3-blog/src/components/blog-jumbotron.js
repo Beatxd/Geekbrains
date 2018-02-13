d5ca1ca5f57ab9db6ccd0bd9751b1b5ee416ecc3
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-13 18:59:34 +0300

    lesson3 blog: complite. 2 layouts bootstrap uglify project add modal

--- React/lesson-3-blog/src/components/blog-jumbotron.js
+++ React/lesson-3-blog/src/components/blog-jumbotron.js
@@ -6,9 +6,8 @@ export default class Jumbotron extends React.Component {
       <div className="jumbotron">
         <h1>Jumbotron heading</h1>
         <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a className="btn btn-lg btn-success" href="#" role="button" data-toggle="modal" data-target="#myModal">Sign up today</a></p>
+        <p><a className="btn btn-lg btn-success" href="#" role="button" data-toggle="modal" data-target="#myModal">Login now!</a></p>
       </div>
-
     )
   }
 }
