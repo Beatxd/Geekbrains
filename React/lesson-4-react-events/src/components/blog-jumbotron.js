bc5bb946e3b535c8a1dd60b129ac3658d094e311
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-17 21:44:02 +0300

    lesson4 react-events: simplify color changer

--- React/lesson-4-react-events/src/components/blog-jumbotron.js
+++ React/lesson-4-react-events/src/components/blog-jumbotron.js
@@ -5,45 +5,51 @@ export default class Jumbotron extends React.Component {
     super(...arguments);
 
     this.state = {
-      showColors: false,
-      color: 'grey'
+      showColors: false
     };
+
+    // functions
+    this.jumbotronStyle = {};
+    this.changeColor = (e) => {
+      switch(e.target.id){
+        case 'btn-green': {
+          this.jumbotronStyle = {backgroundColor: 'lightgreen', color: 'black'};
+          break;
+        }
+        case 'btn-black': {
+          this.jumbotronStyle = {backgroundColor: 'black', color: 'white'};
+          break;
+        }
+        default: this.jumbotronStyle = {}
+      }
+      this.setState({color: 'black'});
+    };
+
+    // binds
+    this.changeColor = this.changeColor.bind(this);
+
   }
   render(){
     // color switcher
     const btnGreen = {backgroundColor: 'green'};
     const btnBlack = {backgroundColor: 'black'};
 
-    let jumbotronStyle = {};
-    switch(this.state.color){
-      case 'green': {
-        jumbotronStyle = {backgroundColor: 'lightgreen', color: 'black'};
-        break;
-      }
-      case 'black': {
-        jumbotronStyle = {backgroundColor: 'black', color: 'white'};
-        break;
-      }
-    }
-
     // btn to open color switcher
     let colorSwitcher;
 
+
     if(this.state.showColors){
       colorSwitcher = (
-        <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" style={btnGreen} className="btn btn-secondary"
-                  onClick={() => {this.setState({color: 'green'})}}>Green</button>
-          <button type="button" style={btnBlack} className="btn btn-secondary"
-                  onClick={() => {this.setState({color: 'black'})}}>Black</button>
-          <button type="button" className="btn btn-secondary"
-                  onClick={() => {this.setState({color: 'grey'})}}>Grey</button>
+        <div className="btn-group" role="group" aria-label="Basic example" onClick={this.changeColor}>
+          <button id="btn-green" type="button" style={btnGreen} className="btn btn-secondary">Green</button>
+          <button id="btn-black" type="button" style={btnBlack} className="btn btn-secondary">Black</button>
+          <button type="button" className="btn btn-secondary">Default</button>
         </div>
       );
     }
 
     return (
-      <div className="jumbotron" style={jumbotronStyle}>
+      <div className="jumbotron" style={this.jumbotronStyle}>
         <h1>Jumbotron heading</h1>
         <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         <p>
