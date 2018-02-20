48585e70ffaa074b77e281d1de2e00edfffb016b
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-20 23:40:27 +0300

    lesson5 react-router: add files from lesson4

---
+++ React/lesson-5-react-router/src/layouts/blog-container.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// components for blog layout
+import BlogHeader from '../components/blog-header';
+import Jumbotron from '../components/blog-jumbotron';
+import RowMarketing from '../components/blog-marketing';
+import Modal from '../components/login-modal';
+
+//data
+import data from '../data/data';
+
+export default class Blog extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Modal/>
+        <BlogHeader/>
+        <Jumbotron styles={data.jumbotronSwitcher}/>
+        <RowMarketing/>
+      </div>
+    )
+  }
+  componentDidMount(){
+    alert('Компонент отрендерен!');
+  }
+}
