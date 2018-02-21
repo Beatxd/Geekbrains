030a7632b5f112b5d321f81891fe0fd07ed7214f
tskon <beatxd@gmail.com>
2018-02-22 00:42:54 +0300

    lesson5 blog-router: restructure components + add modal component

---
+++ React/lesson-5-react-router/src/components/blog-sub-pages/blog-user-posts.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default class Posts extends React.Component {
+  render() {
+    if (!this.props.data.length) return <p>Sorry! No Posts yet</p>;
+
+    let posts = this.props.data.map((post, index) => {
+      if (post.userId === this.props.userId) {
+        return (
+          <div key={'post-' + index}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <p>{'post id: ' + post.id}</p>
+            <hr/>
+          </div>
+        )
+      }
+    });
+
+    return (
+      <div>
+        <button type="button" className="btn btn-outline-info" data-toggle="modal"
+                data-target={"#userModal" + this.props.userId}>
+          Follower posts
+        </button>
+        <div className="modal fade" id={"userModal" + this.props.userId} tabIndex="-1" role="dialog"
+             aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-lg" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">{this.props.userName + '`s posts'}</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                {posts}
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
