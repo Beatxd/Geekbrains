2202bd189c731af83e0af38757d2609444d2bdf3
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-28 23:10:55 +0300

    lesson7-redux: add del method

--- React/lesson-7-redux/src/actions/posts-actions.js
+++ React/lesson-7-redux/src/actions/posts-actions.js
@@ -6,3 +6,10 @@ export function fetchPosts() {
     payload: axios.get('http://jsonplaceholder.typicode.com/posts?userId=1')
   }
 }
+
+export function delPost() {
+  return {
+    type: 'DEL_POST',
+    payload: [{title: 'any pos', body: 'body'}]
+  }
+}
