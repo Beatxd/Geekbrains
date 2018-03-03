6f35531ec8171f30b1aea03d58cedb3e67517d1d
tskon <beatxd@gmail.com>
2018-03-04 01:41:43 +0300

    lesson-8 node api: add ajax requests

--- React/lesson-8-node-api/frontend/js/main.js
+++ React/lesson-8-node-api/frontend/js/main.js
@@ -1,5 +1,15 @@
 window.onload = () => {
-  $.post( "ajax/user/add", function( data ) {
-    $( ".result" ).html( data );
+
+  $.post("ajax/user/getList", function (data) {
+    $(".users-list").html(data);
   });
+
+
+  $('.user-create-form__button').click(() => {
+    const name = $('.user-create-form__name').val();
+    const age = $('.user-create-form__age').val();
+    $.post("ajax/user/add", {name, age});
+  });
+
+
 };
